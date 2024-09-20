c6fba916f1f72435f93bbc3f8ca886e4aca9bd59
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-09-20 22:00:39 +0500

    maldex backend push

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,12 +5,20 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get('DB_NAME'),
+    #     'USER': os.environ.get('DB_USER'),
+    #     'PASSWORD': os.environ.get('DB_PASSWORD'),
+    #     'HOST': os.environ.get('DB_HOST'),
+    #     'PORT': os.environ.get('DB_PORT'),
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': 'maldex',
+        'USER': 'maldex_user',
+        'PASSWORD': 'pass4ord',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
